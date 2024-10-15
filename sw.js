1681b217210d1c818037fbfe74c473f5bdaa1574
Wilmerprox <wsctrabajos@gmail.com>
2024-10-15 16:42:13 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
+            return cache.addAll([
+                'index.html',
+                'cotizacion.html',
+                'css/styles.css',
+                'js/scripts.js',
+                'assets/icon.png' // Asegúrate de tener un ícono en la carpeta de assets
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
